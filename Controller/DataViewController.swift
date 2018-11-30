029a0a6e48eb70a1d36e147a6b6527d88d77afeb
Hurricane36 <43070976+Hurricane36@users.noreply.github.com>
2018-11-30 09:02:04 -0700

    updated dataview

--- Controller/DataViewController.swift
+++ Controller/DataViewController.swift
@@ -12,7 +12,57 @@ private let reuseIdentifier = "Cell"
 
 class DataViewController: UICollectionViewController {
 
-    lazy var bucketLis
+    lazy var bucketList : [BucketItem] =
+    {
+     
+        return loadBucketListFromFile()
+    }()
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        do
+        {
+            if let filePath = Bundle.main.url(forResource: "bucket18" , withExtension: "csv")
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    item.append(BucketItem(contents: item[0], author: item[1]))
+                }
+            }
+        }
+        catch
+        {
+                print("File load error")
+        }
+        return items
+    }
+    
+    //MARK: - Table view data source
+    
+    public override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberofRowsInSection section: Int) -> Int
+    {
+    return bucketList.count
+    }
+    
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier" , for: indexPath) as!
+        BucketItemCell
+        
+        cell.currentBucketItem = bucketList[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
